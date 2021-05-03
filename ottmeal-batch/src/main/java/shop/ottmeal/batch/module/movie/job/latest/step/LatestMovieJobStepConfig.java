eb69bd27c7993918f968874ec48d49f45dbb5b35
iiaii <jhun9409@naver.com>
2021-05-03 23:34:21 +0900

    LatestMovieReader를 RestItemReader 로 대체

--- ottmeal-batch/src/main/java/shop/ottmeal/batch/module/movie/job/latest/step/LatestMovieJobStepConfig.java
+++ ottmeal-batch/src/main/java/shop/ottmeal/batch/module/movie/job/latest/step/LatestMovieJobStepConfig.java
@@ -3,17 +3,32 @@ package shop.ottmeal.batch.module.movie.job.latest.step;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.client.RestTemplate;
+import shop.ottmeal.batch.common.reader.RestItemReader;
+import shop.ottmeal.batch.module.movie.job.latest.dto.LatestMovieResponse;
 
 @Configuration
 @RequiredArgsConstructor
 public class LatestMovieJobStepConfig {
     public static final String STEP_NAME = "latestMovieStep";
 
+    private final RestTemplate restTemplate;
     private final StepBuilderFactory stepBuilderFactory;
     private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    @StepScope
+    public RestItemReader<LatestMovieResponse> latestMovieReader() {
+        return new RestItemReader<>(this.restTemplate,
+                "https://api.themoviedb.org/3/discover/movie?api_key=0b1fe3786795a257dd0648d67445af97",
+                LatestMovieResponse[].class,
+                HttpMethod.GET);
+    }
 
     @Bean(name = STEP_NAME)
     public Step step() {
