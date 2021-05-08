8bc23e6f1ed857d92f689f5734b7a0549fc398b3
iiaii <jhun9409@naver.com>
2021-05-08 23:30:22 +0900

    [batch] movie domain 수정 및 yml 파일 수정

--- ottmeal-batch/src/main/java/shop/ottmeal/batch/controller/MovieController.java
+++ ottmeal-batch/src/main/java/shop/ottmeal/batch/controller/MovieController.java
@@ -1,7 +1,14 @@
 package shop.ottmeal.batch.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import shop.ottmeal.batch.service.MovieService;
 
 @Controller
+@RequiredArgsConstructor
 public class MovieController {
+
+    private final MovieService movieService;
+
+
 }
--- ottmeal-batch/src/main/java/shop/ottmeal/batch/domain/Movie.java
+++ ottmeal-batch/src/main/java/shop/ottmeal/batch/domain/Movie.java
@@ -1,14 +1,20 @@
 package shop.ottmeal.batch.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @Entity
+@Table(name = "movie")
+@Slf4j
 public class Movie {
 
     @Id @GeneratedValue
